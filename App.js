a3f17d078bf172579a5f774885cd8e5029fc1d4e
Murtaza652 <ahmedmurtaza.652@gmail.com>
2023-07-06 18:05:14 +0530

    server

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
 const http = require('http');
 
-const port = 4000;
-
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('My name is Ahmed.\n');
 });
 
-server.listen(port);
+server.listen(4000);
