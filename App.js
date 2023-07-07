9f5571e8ceb65d9634d268ab4b3cc512b6f8711c
Murtaza652 <ahmedmurtaza.652@gmail.com>
2023-07-07 23:32:41 +0530

    message read

--- App.js
+++ App.js
@@ -1,12 +1,48 @@
 const http = require('http');
+const fs= require('fs');
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
 
   const url = req.url;
+  const method = req.method;
 
   // Set the appropriate response based on the URL
-  if (url === '/home') {
+  if(url=== "/") {
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title></head>');
+    fs.readFile('message.txt', 'utf8', (err, data) => {
+      if (!err) {
+        const messages = data.split('\n').filter((message) => message.trim() !== '');
+        res.write('<ul>');
+        messages.forEach((message) => {
+          res.write(`<li>${message}</li>`);
+          console.log(message);
+        });
+        res.write('</ul>');
+      }
+    });
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+    res.write('</html>');
+    return res.end();
+
+  }
+  if(url=== "/message" && method==="POST") {
+    const body=[];
+    req.on("data",(chunk) => {
+      body.push(chunk);
+      console.log(chunk);
+    });
+    return req.on("end",() => {
+      const parsedBody=Buffer.concat(body).toString();
+      const message=parsedBody.split("=")[1];
+      fs.writeFile("message.txt", message, (err)=>{
+        res.statusCode=302;
+        res.setHeader('Location','/');
+        return res.end();
+      });
+    });
+  } else if (url === '/home') {
     res.statusCode = 200;
     res.write('<html>');
     res.write('<head><title>Home Page</title></head>');
