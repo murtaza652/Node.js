19313d8ee64d9ae7bd9ef36d8fa198c87de3abf7
Murtaza652 <ahmedmurtaza.652@gmail.com>
2023-07-07 15:37:55 +0530

    response as per url

--- App.js
+++ App.js
@@ -1,9 +1,37 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('My name is Ahmed.\n');
+  console.log(req.url, req.method, req.headers);
+
+  const url = req.url;
+
+  // Set the appropriate response based on the URL
+  if (url === '/home') {
+    res.statusCode = 200;
+    res.write('<html>');
+    res.write('<head><title>Home Page</title></head>');
+    res.write('<body>Welcome home.</body>');
+    res.write('</html>');
+  } else if (url === '/about') {
+    res.statusCode = 200;
+    res.write('<html>');
+    res.write('<head><title>About Page</title></head>');
+    res.write('<body>Welcome to the About Us page.</body>');
+    res.write('</html>');
+  } else if (url === '/node') {
+    res.statusCode = 200;
+    res.write('<html>');
+    res.write('<head><title>Node</title></head>');
+    res.write('<body>Welcome to my Node.js project.</body>');
+    res.write('</html>');
+  } else {
+    res.statusCode = 404;
+    res.write('<html>');
+    res.write('<head><title>Page Not Found</title></head>');
+    res.write('<body>404 Page Not Found</body>');
+    res.write('</html>');
+  }
+  res.end();
 });
 
 server.listen(4000);
