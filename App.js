edc424df8bf8f5d1913076b38f95e4ea85cda744
Murtaza652 <ahmedmurtaza.652@gmail.com>
2023-08-19 12:15:38 +0530

    express 0.3

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const adminRoutes=require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/add-product",(req, res, next) => {
-    res.send("<form action='/product' method='POST'><input type='text' name='tittle'><input type='number' name='size'><button type='submit'>Add Product </button></form>");
+app.use('/admin',adminRoutes);
+app.use('/shop',shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>404 Not Found</h1>')
 });
 
-app.post("/product",(req, res, next) => {
-   console.log(req.body);
-   res.redirect('/');
-});
-
-app.use("/",(req, res, next) => {
-    res.send('<h1>Welcome to Express!</h1>');
-});
-
-app.listen(4000);  
+app.listen(4000);
